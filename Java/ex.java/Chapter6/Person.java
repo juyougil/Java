bfeb04c039ff48257782b7d1cfdee9956bdf4ffb
juyougil <84062320+juyougil@users.noreply.github.com>
2021-05-27 00:50:32 +0900

    5_26

---
+++ Java/ex.java/Chapter6/Person.java
@@ -0,0 +1,23 @@
+package ex.java.chapter6;
+
+public class Person {
+
+	public static void main(String[] args) {
+		Person_Class d = new Person_Class("최승현", 45, "여", "A", "의사");
+		
+		
+		Person_Class g = new Person_Class("최미녀", 28, "여", "O", "골프선수");
+		
+		
+		Person_Class p = new Person_Class("최미남", 47, "여", "AB", "교수");
+		
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
