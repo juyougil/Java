a82e7e20a90efd0f4fa5e852c9fd3b9a06784078
juyougil <84062320+juyougil@users.noreply.github.com>
2021-05-28 01:46:01 +0900

    5_27

---
+++ Java/Chapter9/Computer.java
@@ -0,0 +1,12 @@
+package kr.java.chapter9;
+
+public abstract class Computer {
+	abstract void display();
+	abstract void typing();
+	
+	
+	
+	public void print() {
+		System.out.println("n");
+	}
+}
---
+++ Java/Chapter9/MyNoteBook.java
@@ -0,0 +1,12 @@
+package kr.java.chapter9;
+
+public class MyNoteBook extends NoteBook {
+
+	@Override
+	void typing() {
+		System.out.println("MyNoteBook typing()");
+		
+	}
+	
+
+}
